6c5bfa35a83a6e8750be4ea0ec9ee8524a40668e
Lodin03 <lamo007@student.kristiania.no>
2025-02-21 15:46:23 +0100

    Første commit av nettsiden, template for hovedsiden, bare frontend kode

---
+++ knipetak/src/App.tsx
@@ -0,0 +1,28 @@
+import "./App.css";
+import logo from "./assets/images/logo.png";
+
+function App() {
+  return (
+    <>
+      <div className="navBar">
+        <img src={logo} alt="LOGO" className="logo" />
+        <div className="navLinks">
+          <p>Hjem</p>
+          <p>Behandlinger</p>
+          <p>Book Time</p>
+          <p>Kontakt</p>
+        </div>
+      </div>
+
+      <div className="mainContent">
+        <h1 className="title">Knipetak - En muskelterapaut på hjul!</h1>
+      </div>
+
+      <div className="footer">
+        <p>post@knipetak.no</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
