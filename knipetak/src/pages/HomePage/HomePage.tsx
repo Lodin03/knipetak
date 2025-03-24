7284a4ee3cd9084c1eaaac3ca76badf136d11708
OlavEL <Olav.t.e@hotmail.com>
2025-03-24 15:12:13 +0100

    Changed Homepage and added Slideshow

--- knipetak/src/pages/HomePage/HomePage.tsx
+++ knipetak/src/pages/HomePage/HomePage.tsx
@@ -1,100 +1,113 @@
-import { useEffect, useState } from "react";
-import { fetchUsers } from "../../backend/firebase/services/firebase.userservice";
-import { UserData } from "../../backend/interfaces/UserData";
-import { onAuthStateChanged, auth, logOut } from "../../backend/firebase/services/firebase.authservice";
+import React from 'react';
 import NavigationBar from '../../components/NavigationBar/NavigationBar';
 import Footer from '../../components/Footer/Footer';
-import './HomePage.css';
+import "../HomePage/HomePage.css"
+import { useEffect, useState } from 'react';
 
-function HomePage() {
-  const [data, setData] = useState<UserData[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
-  const [currentUser, setCurrentUser] = useState<string | null>(null);
-
-  // Effect hook to fetch user data from Firestore when component mounts
+const HomePage: React.FC = () => {
+  const [currentSlide, setCurrentSlide] = useState(0)
+  const slides = ["/src/assets/images/BildeMassasje.jpg",
+"/src/assets/images/Massasje2.jpg"]
   useEffect(() => {
-    const getData = async () => {
-      try {
-        const users = await fetchUsers();
-        setData(users);
-      } catch (err) {
-        setError("Failed to load data. Please try again.");
-      } finally {
-        setLoading(false);
-      }
-    };
-    getData();
-  }, []);
-
-  // Effect hook to get and set the current user's display name from Firebase Auth
-  useEffect(() => {
-    // Subscribe to auth state changes to get the current user's display name
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user?.displayName) {
-        setCurrentUser(user.displayName);
-      } else {
-        setCurrentUser(null);
-      }
-    });
-
-    // Cleanup subscription on unmount
-    return () => unsubscribe();
-  }, []);
-
-   // Function to handle sign out
-   const handleSignOut = async () => {
-    try {
-      await logOut();
-      setCurrentUser(null);
-    } catch (error) {
-      console.error("Error signing out: ", error);
-    }
-   };
+    const slideInterval = setInterval(() => {
+      setCurrentSlide(prev => (prev + 1) % 2)},3000) 
+    return () => clearInterval(slideInterval);
+  }, [])
 
   return (
     <>
-      <NavigationBar />
-      <div className="mainContent">
-        <h1 className="title">Knipetak - En muskelterapeut på hjul!</h1>
-        {/* Testing push to branch*/}
-        
-        {currentUser && (
-          <div className="user-info">
-            <p>Innlogget bruker: {currentUser}</p>
-            <button className="logout-button" onClick={handleSignOut}>Logg ut</button>
-          </div>
-        )}
+    <NavigationBar />
 
-        <div className="firestore">
-          {loading ? (
-            <p>Loading...</p>
-          ) : error ? (
-            <p style={{ color: "red" }}>{error}</p>
-          ) : data.length ? (
-            <div>
-              <h3>Fetched Data fra Firestore:</h3>
-              {data.map((item) => (
-                <div key={item.uid}>
-                  <p>Id: {item.uid}</p>
-                  <p>Email: {item.email}</p>
-                  <p>Alder: {item.age}</p>
-                  <p>Navn: {item.displayName}</p>
-                  <p>Addresse: {item.location?.address}</p>
-                  <p>Helseproblemer: {item.healthIssues}</p>
-                  <p>Telefonnummer: {item.phoneNumber}</p>
-                  <p>Bruker type: {item.userType}</p>
-                </div>
-              ))}
+    <>
+    
+      
+
+     
+      <div className="mainContentHomepage">
+        <h1 className="title">Knipetak - En muskelterapaut på hjul!</h1>
+        <br></br>
+        <div className="ContainerHomePage">
+        <div className="TextContainer">
+        <h3 className="text">Mitt navn er Helene og er muskelterapeut. Jeg er tilgjengelig til å komme der hvor du er. Det vil derfor si at jeg kan komme til deres bedrift eller deres hjem. 
+              Jeg har både benk og stol med meg.
+              <br></br>
+              <br></br>
+              Muskelterapi hjemme er perfekt for de som blant annet har utfordringer med å komme seg ut, og som sliter med diverse muskelproblemer. 
+              <br></br>
+              <br></br>
+              I tillegg møter jeg opp på ulike eventer. Massasje kan være et annerledes og unikt innslag på et event. Jeg kan derfor bookes inn til utdrikningslag og bursdager.</h3>
+          </div>
+          <div className="ImageContainer">
+            <img className="Image" src='/src/assets/images/KnipetakBil.jpg' alt="massasjebilde" height="175px" width= "230px" ></img>
+            <img className="Image" src="/src/assets/images/KnipetakMassasje.jpg" alt="massasjebilde" height="155px" width="230px"></img>
             </div>
-          ) : (
-            <p>No data found.</p>
-          )}
+            <div className="ImageRow">
+            <img className="Image" src="/src/assets/images/KnipetakBilde.jpg" alt="massasjebilde" height="350px" width="240px"></img>
+            </div>
+          
+
+
         </div>
-      </div>
-      <Footer />
+
+
+          
+          
+<br></br>
+
+        <div className="ContainerHomePage2">
+          <div className="TextContainer2">
+            <h3 className="text2">Noen massasjebehandlinger krever en liggende massasje, mens andre massasjebehandlinger krever at du sitter på en stol. Jeg har både stol og benk i bilen. Gi gjerne beskjed hvilken type behandling du ønsker deg. 
+<br></br>
+<br></br>
+Massasje på stol – utføres med klær, uten olje på en ergonomisk tilpasset stol. Enkelt, behagelig og tidseffektivt.
+<br></br>
+<br></br>
+Massasje på benk – utføres med olje /flytende voks (bievoks) direkte på huden. Foregår liggende på behandlerbenk.</h3>
+          </div>
+          <div className="ImageWrap">
+          <div className="ImageContainer2">
+            <img className="Image2" src="/src/assets/images/BildeMassasje.jpg" alt="massasjebilde" height="350px" width="240px"></img>
+            <img className="Image2" src="/src/assets/images/Massasje2.jpg" alt="massasjebilde" height="350px" width="240px"></img>
+          </div>
+          
+          </div> 
+            </div>
+            <div className='SlideShowImage'>
+{slides.map((src, index) => (
+<img
+key = {index}
+className = {`slide ${currentSlide === index ? "active" : ""}`}
+src = {src}
+alt="massasjebilde"
+height="600px"
+width = "350px"
+/>
+
+))}
+</div>
+        </div>
+        
+        <div className='imageSlideshow'>
+        <img className="slide" src="/src/assets/images/BildeMassasje.jpg" alt="massasjebilde" height="500px" width="300px"></img>
+        <img className="slide" src="/src/assets/images/Massasje2.jpg" alt="massasjebilde" height="500px" width="300px"></img>
+
+
+        </div>
+        
+        <script src='SlideShow.js'></script>
+    
+
+
+    
+        
+      
+
+      
+    </>
+  
+    <Footer />
     </>
   );
-}
+};
 
 export default HomePage;
