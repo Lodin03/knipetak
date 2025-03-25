021ca4fa8f89a076d3f46383dcba290ea46384d5
OlavEL <Olav.t.e@hotmail.com>
2025-03-25 15:50:24 +0100

    Added customer reviews aswell as progress bar for the image secuence and some housekeeping

--- knipetak/src/pages/HomePage/HomePage.tsx
+++ knipetak/src/pages/HomePage/HomePage.tsx
@@ -6,33 +6,39 @@ import { useEffect, useState } from 'react';
 
 const HomePage: React.FC = () => {
   const [currentSlide, setCurrentSlide] = useState(0)
+  const [progress, setProgress] = useState(0)
   const slides = ["/src/assets/images/BildeMassasje.jpg",
-"/src/assets/images/Massasje2.jpg"]
+"/src/assets/images/Massasje2.jpg", 
+"/src/assets/images/KnipetakBilde.jpg"]
+
   useEffect(() => {
     const slideInterval = setInterval(() => {
-      setCurrentSlide(prev => (prev + 1) % 2)},3000) 
+      setProgress(prev =>  {
+      if (prev >=  100) {
+      setCurrentSlide((prevSlides) => (prevSlides + 1) % slides.length)
+      return 0;
+      }
+  return prev + 1;
+})
+}, 30)
     return () => clearInterval(slideInterval);
-  }, [])
+  }, [slides.length])
 
   const nextSlides = () => {
+    if (currentSlide < slides.length - 1) {
     setCurrentSlide((prev => (prev + 1 ) % slides.length))
+    }
   }
 
   const prevSlides = () => {
     if (currentSlide > 0) {
-    setCurrentSlide((prev => (prev - 1 + slides.length) % slides.length))
+    setCurrentSlide((prev) => (prev - 1 + slides.length ) % slides.length)
   }
 }
 
   return (
     <>
     <NavigationBar />
-
-    
-    
-      
-
-     
       <div className="mainContentHomepage">
         <h1 className="title">Knipetak - En muskelterapaut på hjul!</h1>
         <br></br>
@@ -54,14 +60,7 @@ const HomePage: React.FC = () => {
             <div className="ImageRow">
             <img className="Image" src="/src/assets/images/KnipetakBilde.jpg" alt="massasjebilde" height="350px" width="240px"></img>
             </div>
-          
-
-
         </div>
-
-
-          
-          
 <br></br>
 
         <div className="ContainerHomePage2">
@@ -83,45 +82,101 @@ Massasje på benk – utføres med olje /flytende voks (bievoks) direkte på hud
           </div> 
             </div>
             <div className='SlideShowImage'>
+             
               <div className='SlideShowButtons'>
             <button onClick={prevSlides}> {"<"} </button>
             </div>
+            <div className='SequenceContainer'>
+            
 {slides.map((src, index) => (
+  
+
 <img
+
 key = {index}
-className = {`slide ${currentSlide === index ? "active" : ""}`}
+className = {`slide ${currentSlide === index ? "active" : "hidden"}`}
 src = {src}
 alt="massasjebilde"
-height="600px"
+height="550px"
 width = "350px"
 />
 
+
 ))}
+<div className='ProgressBarContainer'>
+              <div className='ProgressBar' style={{width: `${progress}%` }}/>
+              </div>
+</div>
 <div className='SlideShowButtons'>
   <button onClick={nextSlides}> {">"} </button>
   </div>
 
+
 </div>
+<div className='Reviews'>
+          <div className='Review'>
+          <p className='author'> Mette</p>
+          <div className='Stars'>
+        <span className='star'>&#9733;</span>
+        <span className='star'>&#9733;</span>
+        <span className='star'>&#9733;</span>
+        <span className='star'>&#9733;</span>
+        <span className='star'>&#9733;</span>
+        </div>
+        <p className='ReviewText'></p>
+        <p className='ReviewText'><strong> Skikkelig fornøyd </strong></p>
+        <br></br>
+        <p className='Feedback'> Helene i Knipetak gir utrolig bra opplevelse både med sin gode massasje og sitt fine vesen. Hun lytter og tilpasser behandling etter dine behov, enten det er plager/smerter eller behandling ifbm å vedlikeholde et aktivt treningsliv.
+Du føler deg sett, trygg og oppnår avslapning og hvile i hennes hender!
+Jeg er skikkelig fornøyd!</p>
+<br></br>
+</div>
+<div className='Review'>
+<p className='author'> Consumer</p>
+<div className='Stars'>
+        <span className='star'>&#9733;</span>
+        <span className='star'>&#9733;</span>
+        <span className='star'>&#9733;</span>
+        <span className='star'>&#9733;</span>
+        <span className='star'>&#9733;</span>
+        </div>
+        <p className='ReviewText'></p>
+        <p className='ReviewText'> <strong> Må oppleves </strong></p>
+        <br></br>
+        <p className='Feedback'> Helene hos Knipetak er utrolig flink og profesjonell. Hun lytter til kunden sine og viser er genuint interessert for å finne årsaken til plagene. Hun har hjulpet meg. Kan virkelig anbefale Helene og knipetak</p>
+        <br></br>
+</div>
+<div className='Review'>
+  <p className='author'> Rune Johansen</p>
+  <div className='Stars'>
+        <span className='star'>&#9733;</span>
+        <span className='star'>&#9733;</span>
+        <span className='star'>&#9733;</span>
+        <span className='star'>&#9733;</span>
+        <span className='star'>&#9733;</span>
+        </div>
+        <p className='ReviewText'> <strong> Kjempe god behandling.</strong></p>
+        <br></br>
+        <p className='Feedback'> 
+          Kjempe god behandling. Lever opp til navnet Knipetak da det ikke var pusebehandling
+Flink dyktig massør som kom til avtalt tid.</p>
+        <br></br>
+</div>
+        </div>
+        <br></br>
         </div>
         
         <div className='imageSlideshow'>
         <img className="slide" src="/src/assets/images/BildeMassasje.jpg" alt="massasjebilde"></img>
         <img className="slide" src="/src/assets/images/Massasje2.jpg" alt="massasjebilde" ></img>
-
+        <img className="slide" src="/src/assets/images/KnipetakBilde.jpg" alt="massasjebilde" ></img>
 
         </div>
         
         <script src='SlideShow.js'></script>
-    
 
-
-    
         
       
-
-      
-    
-  
     <Footer />
     </>
   );
