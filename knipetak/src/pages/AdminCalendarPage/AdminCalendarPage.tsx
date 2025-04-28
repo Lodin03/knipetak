1c5c5ec31ec65c4649c3eef0a0b6d4cf8446e070
Oskar Fritzner <72510834+oskarfritzner@users.noreply.github.com>
2025-04-28 08:36:45 +0200

    fikset google kalenderen ferdig

--- knipetak/src/pages/AdminCalendarPage/AdminCalendarPage.tsx
+++ knipetak/src/pages/AdminCalendarPage/AdminCalendarPage.tsx
@@ -6,7 +6,6 @@ const AdminCalendarPage: React.FC = () => {
   return (
     <>
       <div className="calendar-container">
-        <h1 className="calendar-title">Google Kalender Oversikt</h1>
         <div className="calendar-section">
           <AdminGoogleCalendar />
         </div>
